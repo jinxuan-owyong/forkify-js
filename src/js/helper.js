2441c319b93e818fc589f7cb62edbfb263bcf948
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-05-01 18:34:07 +0800

    Added config and helpers

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,7 +1,23 @@
+import { TIMEOUT_DELAY } from './config';
+
 export const timeout = function (s) {
   return new Promise(function (_, reject) {
     setTimeout(function () {
       reject(new Error(`Request took too long! Timeout after ${s} second`));
     }, s * 1000);
   });
+};
+
+export const getJSON = async function (url) {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const res = await Promise.race([fetch(url), timeout(TIMEOUT_DELAY)]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    return data;
+  } catch (err) {
+    throw err;
+  }
 };
