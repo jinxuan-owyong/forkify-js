37725bccf86834949e1c4cce88f941795534e177
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-05-01 17:39:48 +0800

    Refactor code for MVC architecture

---
+++ src/js/model.js
@@ -0,0 +1,29 @@
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    // Process recipe data
+    const { recipe } = data.data;
+    state.recipe = {
+      cookingTime: recipe.cooking_time,
+      id: recipe.id,
+      image: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      title: recipe.title,
+    };
+  } catch (err) {
+    alert(err);
+  }
+};
