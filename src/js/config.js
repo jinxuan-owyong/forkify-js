ea5ff484f592e35630e4a2933bd11d59ff03798d
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-05-05 21:50:02 +0800

    Add .env for API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const API_KEY = process.env.FORKIFY_API_KEY;
 export const TIMEOUT_DELAY = 5; // seconds
 export const RESULTS_PER_PAGE = 10;
